ec60285f92e77469c49d94abbaf767d893fe6fef
Donatas Noreika <donoras@gmail.com>
2022-10-27 09:00:52 +0300

    aprašyti modeliai

--- api_example/bands_api/models.py
+++ api_example/bands_api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Band(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=200)
+
+
+class Album(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=200)
+    band = models.ForeignKey(to="Band", on_delete=models.CASCADE)
+
+
+class Song(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=200)
+    duration = models.DurationField(verbose_name="Duration")
+    album = models.ForeignKey(to="Album", on_delete=models.CASCADE)
+
+
+class AlbumReview(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    album = models.ForeignKey(to="Album", on_delete=models.CASCADE)
+    content = models.TextField(verbose_name="Content", max_length=3000, null=True, blank=True)
+    score = models.IntegerField(verbose_name="Score")
+
+
+class AlbumReviewLike(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    album_review = models.ForeignKey(to=AlbumReview, on_delete=models.CASCADE)
