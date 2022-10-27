f1669d90344b38185d1508ea43ae9906a212dd75
Donatas Noreika <donoras@gmail.com>
2022-10-27 09:40:03 +0300

    aprašyti modelių str, admin puslapis

--- api_example/bands_api/admin.py
+++ api_example/bands_api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (Band,
+                     Song,
+                     Album,
+                     AlbumReview,
+                     AlbumReviewLike)
 
 # Register your models here.
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Song)
+admin.site.register(Album)
+admin.site.register(AlbumReview)
+admin.site.register(AlbumReviewLike)
--- api_example/bands_api/models.py
+++ api_example/bands_api/models.py
@@ -6,16 +6,25 @@ from django.contrib.auth.models import User
 class Band(models.Model):
     name = models.CharField(verbose_name='Name', max_length=200)
 
+    def __str__(self):
+        return f"Band {self.name}"
+
 
 class Album(models.Model):
     name = models.CharField(verbose_name='Name', max_length=200)
     band = models.ForeignKey(to="Band", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Album {self.name} of band {self.band.name}"
 
 
 class Song(models.Model):
     name = models.CharField(verbose_name='Name', max_length=200)
     duration = models.DurationField(verbose_name="Duration")
     album = models.ForeignKey(to="Album", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Song {self.name} of album {self.album.name}"
 
 
 class AlbumReview(models.Model):
@@ -24,7 +33,13 @@ class AlbumReview(models.Model):
     content = models.TextField(verbose_name="Content", max_length=3000, null=True, blank=True)
     score = models.IntegerField(verbose_name="Score")
 
+    def __str__(self):
+        return f"Album {self.album.name} of album {self.album.band.name} review"
+
 
 class AlbumReviewLike(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     album_review = models.ForeignKey(to=AlbumReview, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"User {self.user} like of {self.album_review.id} review"
